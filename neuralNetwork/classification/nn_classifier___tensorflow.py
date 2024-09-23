5b3fdbf1bc49d68d7deb85adfae213dc22b5d645
Sandro0212 <sandro.malusa@optimad.it>
2024-09-23 15:22:47 +0200

    nn::classification::tensorflow model and training results

---
+++ neuralNetwork/classification/nn_classifier___tensorflow.py
@@ -0,0 +1,192 @@
+# Multilayer perceptron classifier for Titanic survivors dataset.
+# ML library: Tensorflow.
+ 
+import pandas as pd
+import numpy as np
+import seaborn as sns 
+import matplotlib.pyplot as plt
+# nn libraries
+import tensorflow as tf
+from tensorflow import keras 
+from keras.layers import Dense, Dropout, Input
+from keras.models import Sequential
+
+# load data
+train_data = pd.read_csv('./train.csv')
+test_data = pd.read_csv('./test.csv')
+
+print("Total number of rows in training data ", train_data.shape[0])
+print("Total number of columns in training data ", train_data.shape[1])
+print("Total number of rows in test data ", test_data.shape[0])
+print("Total number of columns in test data ", test_data.shape[1])
+'''
+plt.figure(figsize = (13,5))
+plt.bar(train_data.columns, train_data.isna().sum())
+plt.xlabel("Columns name")
+plt.ylabel("Number of missing values in training data")
+plt.show()
+
+plt.figure(figsize = (13,5))
+plt.bar(test_data.columns, test_data.isnull().sum().values, color = 'red')
+plt.xlabel("Columns name")
+plt.ylabel("Number of missing values in test data")
+plt.show()
+'''
+# drop the cabin column
+train_data.drop('Cabin', axis = 1, inplace = True)
+test_data.drop('Cabin', axis = 1, inplace = True)
+
+# missing values
+combined_data = [train_data, test_data]
+for data in combined_data:
+    print(data.isnull().sum())
+    print('*' * 20)
+
+for data in combined_data:
+    data.Age.fillna(data.Age.mean(), inplace = True)
+    data.Fare.fillna(data.Fare.mean(), inplace = True)
+    
+# from visualization we know that Southamptom is most frequent Embarked place so, filling the missing value with 'S'
+train_data.Embarked.fillna('S', inplace = True)
+
+# we simply can use SimpleImputer class form the sklearn to deal with the missing value
+# from sklearn.impute import SimpleImputer
+# impute = SimpleImputer(missing_values = np.nan, strategy = 'mean')
+# train_data[['Age']] = impute.fit_transform(train_data[['Age']])
+
+# convert categorical values
+def change_gender(x):
+    if x == 'male':
+        return 0
+    elif x == 'female':
+        return 1
+train_data.Sex = train_data.Sex.apply(change_gender)
+test_data.Sex = test_data.Sex.apply(change_gender)
+# we simply can use mapfunction to change the gender
+# train_data.Sex = train_data.Sex.map({'female':1, 'male':0})
+
+change = {'S':1,'C':2,'Q':0}
+train_data.Embarked = train_data.Embarked.map(change)
+test_data.Embarked = test_data.Embarked.map(change)
+
+# we know that Sibsp and Parch columns were closely related 
+# so lets created new column called Alone using this two columns -------> 1 = Alone , 0 = not Alone
+train_data['Alone'] = train_data.SibSp + train_data.Parch
+test_data['Alone'] = test_data.SibSp + test_data.Parch
+
+train_data.Alone = train_data.Alone.apply(lambda x: 1 if x == 0 else 0)
+test_data.Alone = test_data.Alone.apply(lambda x: 1 if x == 0 else 0)
+
+# now lets drop SibSp and Parch column for both training and testing data
+train_data.drop(['SibSp','Parch'], axis = 1, inplace = True)
+test_data.drop(['SibSp','Parch'], axis = 1, inplace = True )
+
+train_data.Name.str.extract(' ([A-Za-z]+)\.', expand=False).unique().size
+
+for data in combined_data:
+    data['Title'] = data.Name.str.extract('([A-Za-z]+)\.', expand = False)
+    data.drop('Name', axis = 1, inplace = True)
+
+train_data.Title.value_counts()
+test_data.Title.unique()
+least_occuring = [ 'Don', 'Rev', 'Dr', 'Mme', 'Ms',
+       'Major', 'Lady', 'Sir', 'Mlle', 'Col', 'Capt', 'Countess','Dona',
+       'Jonkheer']
+for data in combined_data:
+    data.Title = data.Title.replace(least_occuring, 'Rare')
+
+title_mapping = {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Rare": 5}
+for data in combined_data:
+    data['Title'] = data['Title'].map(title_mapping)
+columns_to_drop = ['PassengerId','Ticket']
+train_data.drop(columns_to_drop, axis = 1, inplace = True)
+test_data.drop(columns_to_drop[1], axis = 1, inplace = True)
+
+# binning age and fare columns
+for dataset in combined_data:    
+    dataset.loc[ dataset['Age'] <= 16, 'Age'] = 0
+    dataset.loc[(dataset['Age'] > 16) & (dataset['Age'] <= 32), 'Age'] = 1
+    dataset.loc[(dataset['Age'] > 32) & (dataset['Age'] <= 48), 'Age'] = 2
+    dataset.loc[(dataset['Age'] > 48) & (dataset['Age'] <= 64), 'Age'] = 3
+    dataset.loc[ dataset['Age'] > 64, 'Age'] = 4
+
+for data in combined_data:
+    data.loc[data['Fare'] < 30, 'Fare'] = 1
+    data.loc[(data['Fare'] >= 30) & (data['Fare'] < 50),'Fare'] = 2
+    data.loc[(data['Fare'] >= 50) & (data['Fare'] < 100),'Fare'] = 3
+    data.loc[(data['Fare'] >= 100),'Fare'] = 4
+
+# training and testing data
+X_train = train_data.drop("Survived", axis=1)
+Y_train = train_data["Survived"]
+X_test = test_data.drop("PassengerId", axis = 1)
+print("shape of X_train",X_train.shape)
+print("Shape of Y_train",Y_train.shape)
+print("Shape of x_test",X_test.shape)
+print(X_train.head())
+print(Y_train.head())
+
+# neural network
+model = Sequential()
+model.add(Dense(units = 32, input_shape = (7,), activation = 'relu'))
+model.add(Dense(units = 64, activation = 'relu', kernel_initializer = 'he_normal', use_bias = False))
+model.add(tf.keras.layers.BatchNormalization())
+model.add(Dense(units = 128, activation = 'relu',kernel_initializer = 'he_normal', use_bias = False))
+model.add(Dropout(0.01))
+model.add(Dense(units = 64, activation = 'relu',kernel_initializer = 'he_normal', use_bias = False))
+model.add(Dropout(0.01))
+model.add(Dense(units = 32, activation = 'relu'))
+model.add(Dropout(0.01))
+model.add(Dense(units = 16, activation = 'relu'))
+model.add(Dense(units = 8, activation = 'relu',kernel_initializer = 'he_normal', use_bias = False))
+model.add(Dense(units =1 , activation = 'sigmoid'))
+
+model.summary()
+
+# Compiling the model uses the efficient numerical libraries under the covers (the so-called backend) such as Theano or TensorFlow.
+# The backend automatically chooses the best way to represent the network for training and making predictions 
+# to run on your hardware, such as CPU, GPU, or even distributed.
+adam = tf.keras.optimizers.Adam(learning_rate = 0.0001)
+model.compile(loss = tf.keras.losses.binary_crossentropy, optimizer = adam, metrics = ['acc'])
+
+#create callback
+from keras.callbacks import ModelCheckpoint
+filepath = 'tensorflow_model.keras'
+checkpoint = ModelCheckpoint(filepath=filepath, 
+                             monitor='val_loss',
+                             verbose=0, 
+                             save_best_only=True,
+                             mode='min')
+
+# fit the model
+history = model.fit(X_train, Y_train, batch_size = 30, validation_split = 0.15, verbose = 1, epochs = 100, callbacks = [checkpoint])
+
+# plot training history
+plt.figure(figsize = (13,5))
+plt.plot(history.history['loss'], label='Training Loss')
+plt.plot(history.history['val_loss'], label='Validation Loss')
+plt.legend()
+plt.xlabel('Epochs')
+plt.ylabel('Mean Squared Error')
+plt.savefig('model_training_history')
+plt.savefig('./training_loss', dpi = 300)
+plt.close()
+
+
+predict = model.predict(X_test)
+
+# since we use sigmoid activation function in output layer
+predict = (predict > 0.5).astype(int).ravel()
+
+from sklearn import metrics
+Y_pred_rand = (model.predict(X_train) > 0.5).astype(int)
+print('Precision : ', np.round(metrics.precision_score(Y_train, Y_pred_rand)*100,2))
+print('Accuracy : ', np.round(metrics.accuracy_score(Y_train, Y_pred_rand)*100,2))
+print('Recall : ', np.round(metrics.recall_score(Y_train, Y_pred_rand)*100,2))
+print('F1 score : ', np.round(metrics.f1_score(Y_train, Y_pred_rand)*100,2))
+print('AUC : ', np.round(metrics.roc_auc_score(Y_train, Y_pred_rand)*100,2))
+
+# plotting the confusion matrix in heatmap
+#matrix = metrics.confusion_matrix(Y_train, Y_pred_rand)
+#sns.heatmap(matrix, annot = True,fmt = 'g')
+#plt.show()
